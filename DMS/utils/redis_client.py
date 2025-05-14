b97baa2aa9527110451b9b69d8d285401517b02e
Walter Lazo <walterlazo293@gmail.com>
2025-05-13 23:05:25 -0600

    Revert "Merge pull request #1 from Walter-Lz/backend"
    This reverts commit a5ccbbea67c1f8d8d0ae626dadda3ac8ff726d41, reversing
    changes made to df87d81c2d4014d5a3a57ce405786748a1a9e884.

--- DMS/utils/redis_client.py
+++ DMS/utils/redis_client.py
@@ -1,7 +1,5 @@
 import redis
-import os
 
 def get_redis():
-    host = os.getenv('REDIS_HOST', 'localhost')
-    port = int(os.getenv('REDIS_PORT', 6379))
-    return redis.Redis(host=host, port=port, decode_responses=True)
+    r= redis.Redis(host="localhost", port=6379, decode_responses=True)
+    return r
