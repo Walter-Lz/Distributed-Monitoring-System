74343789d2785bc39d4279bab7a188de8e259d90
Steven Chacón <stechacon@estudiantec.cr>
2025-05-18 22:01:39 -0600

    BiggerModal2

--- web/src/app/component/NodeModal.tsx
+++ web/src/app/component/NodeModal.tsx
@@ -106,7 +106,7 @@ export default function NodeModal({ isOpen, onClose, nodeId, stats }: Props) {
           <button
             onClick={onClose}
             className="mt-4 px-4 py-2 bg-green-500 hover:bg-green-600 rounded text-white w-full transition-colors"
-          >
+          > 
             Close
           </button>
         </Dialog.Panel>
